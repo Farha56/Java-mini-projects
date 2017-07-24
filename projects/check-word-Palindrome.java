3e7adbff4755fac2e121185ae48d5f0b2f9c0bc2
Mohamed Abdulfatah <ce@mabdulfatah.com>
2017-07-24 19:27:25 +0200

    Add files via upload

---
+++ projects/check-word-Palindrome.java
@@ -0,0 +1,27 @@
+package com.company;
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] word={"l","e","v","e","l"};
+        int count=0,j=4;
+
+
+        for (int i = 0; i <2 ; i++) {
+
+            if (word[i]==word[j]){
+                count++;
+                j--;
+            }
+        } //End Loop
+
+
+        if (count==2){
+            System.out.println("ITS A PALINDROME");
+        }
+        else {
+            System.out.println("ITS NOT A PALINDROME");
+        }
+
+    }
+}
